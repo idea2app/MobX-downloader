65d45b7937d0475c2f8a76eeb185f4010544e37a
TechQuery <TechQuery@users.noreply.github.com>
2024-10-29 10:58:00 +0000

    deploy: ef32d4222a0bee9600acff219b33c7b8444563b3

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyyS/Py8lPTEktUtJRKkgsyVCyUkrOSSwuTi3WR8jpZZTk5ijpKGVn5qUoWRkaWdTqYJgQklicjdsMkCw+UzxCQgLwm4SuAtO0WADGinQU1wAAAA=="
